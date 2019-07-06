c5f539c7f724e4e5653a42e62626495cffe2f699
Niklas Ekman <nikl.ekman@gmail.com>
2019-07-06 20:33:57 +0200

    add testcases for fallback

--- tests/SamplingRule/AwsSdkSamplingRuleRepositoryTest.php
+++ tests/SamplingRule/AwsSdkSamplingRuleRepositoryTest.php
@@ -3,6 +3,7 @@ namespace Pkerrigan\Xray\SamplingRule;
 
 use PHPUnit\Framework\TestCase;
 use Aws\XRay\XRayClient;
+use Aws\Exception\AwsException;
 
 class AwsSdkSamplingRuleRepositoryTest extends TestCase
 {
@@ -36,6 +37,43 @@ class AwsSdkSamplingRuleRepositoryTest extends TestCase
         ];
         
         $this->assertEquals($expected, $repository->getAll());
+    }
+    
+    public function testGetAllAwsErrorWithFallback()
+    {
+        $exception = $this->createMock(AwsException::class);
+        
+        $xrayClient = $this->createMock(XRayClient::class);
+        $xrayClient->expects($this->once())
+            ->method('getPaginator')
+            ->with($this->equalTo('GetSamplingRules'))
+            ->will($this->throwException($exception));
+        
+        $fallbackSamplingRule = [ 'my_fake_fallback_sampling_rule' ];
+            
+        $repository = new AwsSdkSamplingRuleRepository($xrayClient, $fallbackSamplingRule);
+        
+        $samplingRules = $repository->getAll();
+        
+        $this->assertCount(1, $samplingRules);
+        $this->assertEquals($fallbackSamplingRule, $samplingRules[0]);
+    }
+    
+    public function testGetAllAwsErrorWithoutFallback()
+    {
+        $exception = $this->createMock(AwsException::class);
+        
+        $this->expectException(get_class($exception));
+        
+        $xrayClient = $this->createMock(XRayClient::class);
+        $xrayClient->expects($this->once())
+            ->method('getPaginator')
+            ->with($this->equalTo('GetSamplingRules'))
+            ->will($this->throwException($exception));
+        
+        $repository = new AwsSdkSamplingRuleRepository($xrayClient);
+        
+        $repository->getAll();
     }
 }
 
