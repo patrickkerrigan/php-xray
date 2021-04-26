e421d8af3372a6f166c8a990a4625ba51b24e72e
Pavel Kirpitsov <pavel.kirpichyov@gmail.com>
2021-04-26 15:21:26 +0300

    feat: allow setup aws.account_id for segments & subsegments

--- tests/HttpSegmentTest.php
+++ tests/HttpSegmentTest.php
@@ -16,7 +16,8 @@ class HttpSegmentTest extends TestCase
         $segment = new HttpSegment();
         $segment->setUrl('http://example.com/')
             ->setMethod('GET')
-            ->setResponseCode(200);
+            ->setResponseCode(200)
+            ->setAwsAccountId(123);
 
         $serialised = $segment->jsonSerialize();
 
@@ -24,5 +25,6 @@ class HttpSegmentTest extends TestCase
         $this->assertEquals('http://example.com/', $serialised['http']['request']['url']);
         $this->assertEquals('GET', $serialised['http']['request']['method']);
         $this->assertEquals(200, $serialised['http']['response']['status']);
+        $this->assertEquals(123, $serialised['aws']['account_id']);
     }
 }
